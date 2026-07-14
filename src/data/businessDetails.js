6e536a511436c32c033eb1b9e536c933062f9d33
mickael31 <mickael.cambucco@gmail.com>
2026-07-14 16:22:43 +0200

    feat(seo): add confirmed business details

---
+++ src/data/businessDetails.js
@@ -0,0 +1,22 @@
+export const businessDetails = {
+  legalName: "Monsieur Camille Mesnier",
+  siren: "952227486",
+  siret: "95222748600018",
+  email: "contact@location-benne-occitanie.fr",
+  openingHoursLabel: "Tous les jours de 07:00 à 20:00",
+  openingHours: [
+    {
+      dayOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      opens: "07:00",
+      closes: "20:00",
+    },
+  ],
+};
